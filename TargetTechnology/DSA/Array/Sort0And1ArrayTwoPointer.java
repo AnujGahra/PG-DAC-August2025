5a148581734b64c607974c0403970e50a025c76e
Anuj Kumar <anujgahra244304@gmail.com>
2026-01-25 13:04:08 +0530

    sortZeroesAndOnes

--- TargetTechnology/DSA/Array/Sort0And1ArrayTwoPointer.java
+++ TargetTechnology/DSA/Array/Sort0And1ArrayTwoPointer.java
@@ -65,6 +65,26 @@ public class Sort0And1ArrayTwoPointer {
             }
         }
     }
+
+
+    static int[] sortSquares(int[] arr) {
+        int n = arr.length;
+        int left = 0, right = n-1;
+
+        int[] ans = new int[n];
+        int k = 0;
+
+        while(left <= right) {
+            if (Math.abs(arr[left]) > Math.abs(arr[right])) {
+                ans[k++] = arr[left] * arr[left];
+                left++;
+            } else {
+                ans[k++] = arr[right] * arr[right];
+                right--;
+            }
+            return ans;
+        }
+    }
     
 
     public static void main(String[] args) {
