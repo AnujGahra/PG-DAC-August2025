846636bf38d889b8c898e33ad908289848e71884
Anuj Kumar <anujgahra244304@gmail.com>
2026-01-26 12:28:17 +0530

    Anagram

--- TargetTechnology/DSA/Array/AnagramString.java
+++ TargetTechnology/DSA/Array/AnagramString.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class AnagramString {
     
 
@@ -21,6 +23,22 @@ public class AnagramString {
 
         return true;
     }
+
+    // Using Sorting
+    public static boolean areAnagramsUsingSorting(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+
 
     public static void main(String[] args) {
         String str1 = "listen";
