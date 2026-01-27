1a7426bd59d443c8f6790b64776b809050ed2a7f
Anuj Kumar <anujgahra244304@gmail.com>
2026-01-27 10:25:32 +0530

    Bulk Row CRUD Operation using Native SQL in JPAAPI

--- TargetTechnology/Java_Based/Hibernate/HBN-5/src/com/mainapp/Launch.java
+++ TargetTechnology/Java_Based/Hibernate/HBN-5/src/com/mainapp/Launch.java
@@ -11,21 +11,19 @@ import javax.persistence.Query;
 import com.entity.Employee;
 
 public class Launch {
-	
+
 //	NATIVE SQL
 
 	public static void main(String[] args) {
-		
-		
+
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("config");
 		EntityManager em = entityManagerFactory.createEntityManager();
-		
-		
+
 //		inserted(em);  
 		read(em);
 //		update(em);
 //		delete(em);
-		
+
 		em.close();
 		entityManagerFactory.close();
 	}
@@ -33,46 +31,51 @@ public class Launch {
 	private static void delete(EntityManager em) {
 		EntityTransaction transaction = em.getTransaction();
 		transaction.begin();
-		
-		
-		
-		
-		
+		Query query = em.createNamedQuery("deleteSQL");
+		query.setParameter("id", 117);
+		query.executeUpdate();
+
 		transaction.commit();
-		
+		System.out.println("BULK DATA DELETED");
+		em.close();
+
 	}
 
 	private static void update(EntityManager em) {
 		EntityTransaction transaction = em.getTransaction();
 		transaction.begin();
-		
-		
-		
-		
-		
+
+		String sql = "UPDATE xemployee " + "SET salary = ? " + "WHERE id >= ?";
+
+		Query query = em.createNativeQuery(sql);
+
+		query.setParameter(1, 98989898);
+		query.setParameter(2, 12347);
+
+		query.executeUpdate();
+
 		transaction.commit();
-		
+
+		System.out.println("BULK DATA UPDATED");
+
+		em.close();
+
 	}
 
 	private static void read(EntityManager em) {
-		
+
 		// ------------------------------
 		// Approach 1: Native Query without Entity Mapping
 		// ------------------------------
 		/*
-		String sql = "SELECT * FROM xemployee";
-
-		Query query = em.createNativeQuery(sql);
-		List<Object[]> list = query.getResultList();
-
-		for (Object[] row : list) {
-		    for (Object column : row) {
-		        System.out.print(column + " ");
-		    }
-		    System.out.println();
-		}
-		*/
-
+		 * String sql = "SELECT * FROM xemployee";
+		 * 
+		 * Query query = em.createNativeQuery(sql); List<Object[]> list =
+		 * query.getResultList();
+		 * 
+		 * for (Object[] row : list) { for (Object column : row) {
+		 * System.out.print(column + " "); } System.out.println(); }
+		 */
 
 		// ------------------------------
 		// Approach 2: Native Query with Entity Mapping (Recommended)
@@ -83,7 +86,7 @@ public class Launch {
 		List<Employee> list = query.getResultList();
 
 		for (Employee employee : list) {
-		    System.out.println(employee);
+			System.out.println(employee);
 		}
 
 		em.close();
@@ -91,25 +94,22 @@ public class Launch {
 	}
 
 	private static void inserted(EntityManager em) {
-		
-		
+
 		EntityTransaction transaction = em.getTransaction();
 		transaction.begin();
 
-		String sql =
-		        "INSERT INTO xemployee (id, name, address, salary) " +
-		        "VALUES (?, ?, ?, ?)";
+		String sql = "INSERT INTO xemployee (id, name, address, salary) " + "VALUES (?, ?, ?, ?)";
 
 		for (int i = 1; i <= 10; i++) {
 
-		    Query nativeQuery = em.createNativeQuery(sql);
+			Query nativeQuery = em.createNativeQuery(sql);
 
-		    nativeQuery.setParameter(1, 12345 + i);
-		    nativeQuery.setParameter(2, "xyzijkl");
-		    nativeQuery.setParameter(3, "adddrrr");
-		    nativeQuery.setParameter(4, 676754 + i);
+			nativeQuery.setParameter(1, 12345 + i);
+			nativeQuery.setParameter(2, "xyzijkl");
+			nativeQuery.setParameter(3, "adddrrr");
+			nativeQuery.setParameter(4, 676754 + i);
 
-		    nativeQuery.executeUpdate();
+			nativeQuery.executeUpdate();
 		}
 
 		transaction.commit();
@@ -117,7 +117,6 @@ public class Launch {
 
 		System.out.println("BULK DATA INSERTED");
 
-		
 	}
 
 }
