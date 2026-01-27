68fef787393c8bd3cbbd5a578c31bb6eeac13af4
Anuj Kumar <anujgahra244304@gmail.com>
2026-01-27 23:33:37 +0530

    Spring-Security Basics Authentication

--- Web_Technology/Java_Based/Spring_Security/Security-1/src/main/java/com/codehunt/SecurityConfig.java
+++ Web_Technology/Java_Based/Spring_Security/Security-1/src/main/java/com/codehunt/SecurityConfig.java
@@ -1,9 +1,12 @@
-package com.codehunt;
+ package com.codehunt;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -14,10 +17,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-            .authorizeRequests()
-                .antMatchers("/member", "/admin").authenticated()
-                .anyRequest().permitAll()
-            .and()
-            .httpBasic();   
+        	.authorizeHttpRequests()
+        	.antMatchers("/member").hasRole("MEMBER")
+        	.antMatchers("/admin").hasRole("ADMIN")
+        	.anyRequest().permitAll()
+        	.and()
+        	.httpBasic();
+//            .authorizeRequests()
+//                .antMatchers("/member", "/admin").authenticated()
+//                .anyRequest().permitAll()
+//            .and()
+//            .httpBasic();   
+    }
+    
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	auth
+    		.inMemoryAuthentication()
+    		.withUser("admin123")
+    		.password(bCryptPasswordEncoder().encode("admin333"))
+    		.roles("ADMIN")
+    		.and()
+    		.withUser("member123")
+    		.password(bCryptPasswordEncoder().encode("member333"))
+    		.roles("MEMBER");
+    		
+    }
+    
+    
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    	return new BCryptPasswordEncoder();
     }
 }
