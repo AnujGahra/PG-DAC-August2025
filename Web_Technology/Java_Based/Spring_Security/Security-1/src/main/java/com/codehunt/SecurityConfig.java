f83a2c47258408527ce038c33bfbb3339e3704c1
Anuj Kumar <anujgahra244304@gmail.com>
2026-01-29 11:41:55 +0530

    Spring Security Basic Authentication using Database

--- Web_Technology/Java_Based/Spring_Security/Security-1/src/main/java/com/codehunt/SecurityConfig.java
+++ Web_Technology/Java_Based/Spring_Security/Security-1/src/main/java/com/codehunt/SecurityConfig.java
@@ -1,5 +1,6 @@
  package com.codehunt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,10 +9,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.codehunt.service.MyService;
+
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private MyService myService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     	
     	
-    	
+    	auth.userDetailsService(myService).passwordEncoder(bCryptPasswordEncoder());
     	
     	
 //    	In Memeory Authentication
--- Web_Technology/Java_Based/Spring_Security/Security-1/src/main/java/com/codehunt/service/MyService.java
+++ Web_Technology/Java_Based/Spring_Security/Security-1/src/main/java/com/codehunt/service/MyService.java
@@ -1,9 +1,39 @@
 package com.codehunt.service;
 
-public class MyService {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.codehunt.UserDetailsImpl;
+import com.codehunt.dao.UserRepo;
+import com.codehunt.entity.User;
+
+
+@Service
+public class MyService implements UserDetailsService {
 	
-	public void readData() {
+	@Autowired
+	private UserRepo userRepo;
+	
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		System.out.println(username);
+		Optional<User> opt = userRepo.findByUsername(username);
 		
+		if(opt.isPresent()) {
+			User user = opt.get();
+			return new UserDetailsImpl(user);
+		} else {
+//			Throw exceptions
+			throw new UsernameNotFoundException("User Not Found");
+		}  
+		return null;
 	}
 
 }
