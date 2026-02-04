1cad1c332af63d0f7eceb548a91f3db62fa40de3
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-04 21:58:37 +0530

    Inline function in C++

---
+++ TargetTechnology/C++/InlineFunction.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+inline int getSum(int a, int b) {
+    return a + b;
+}
+
+int main() {
+
+    int num1 = 10, num2 = 20;
+    cout << "Sum: " << getSum(num1, num2) << endl;
+    return 0;
+
+}
