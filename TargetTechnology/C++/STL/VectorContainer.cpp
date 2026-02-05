3e045291e0fa173530b281bc8d35ec255dad99d4
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-05 12:28:04 +0530

    STL in C++

--- TargetTechnology/C++/STL/VectorContainer.cpp
+++ TargetTechnology/C++/STL/VectorContainer.cpp
@@ -11,6 +11,9 @@ int main() {
     vec.push_back(30);
     vec.push_back(40);
     vec.push_back(50);
+
+
+    vec.emplace_back(60);
     cout << "Size: " << vec.size() << endl;
     cout << "Capacity: " << vec.capacity() << endl;
 
