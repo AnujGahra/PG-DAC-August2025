759522bc161be993e5ea0c5e7ae08f4df0ba750f
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-05 11:13:20 +0530

    Template in C++

--- TargetTechnology/C++/template.cpp
+++ TargetTechnology/C++/template.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 template <typename T>
-T sum(T a, T b) {
+T sum(T a, T b)
+{
     return a + b;
 }
 
-
-int main() {
-    cout << "Sum of 10 and 20: " << sum(10, 20) << endl;
-    cout << "Sum of 5.5 and 4.5: " << sum(5.5, 4.5) << endl;
+int main()
+{
+    cout << "Sum of 10 and 20: " << sum<int>(10, 20) << endl;
+    cout << "Sum of 5.5 and 4.5: " << sum<double>(5.5, 4.5) << endl;
     return 0;
 }
