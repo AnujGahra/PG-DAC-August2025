1f598f7a085aada7250c26c31908d0fcac193b09
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-11 21:50:21 +0530

    Spring Boot => SpringBoot Data JPA JpaRepository

--- TargetTechnology/Java_Based/SpringBoot/SB-2/src/main/java/com/example/demo/Sb2Application.java
+++ TargetTechnology/Java_Based/SpringBoot/SB-2/src/main/java/com/example/demo/Sb2Application.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,7 +16,19 @@ public class Sb2Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ac = SpringApplication.run(Sb2Application.class, args);
 		EmployeeCrud bean = ac.getBean(EmployeeCrud.class);
-		bean.save(new EmployeeEntity(11, "raju", "add1", 1100));
+//		bean.save(new EmployeeEntity(11, "raju", "add1", 1100));
+		
+		bean.save(new EmployeeEntity(12, "raju12", "add12", 1200));
+		bean.save(new EmployeeEntity(13, "raju13", "add13", 1300));
+
+//		for read
+//		List<EmployeeEntity> list = bean.findAll();
+//		for(EmployeeEntity emp : list) 
+//			System.out.println(emp);  
+		
+		Optional<EmployeeEntity> byId = bean.findById(12);
+		EmployeeEntity employeeEntity = byId.get();
+		System.out.println(employeeEntity);
 		
 		System.out.println("Application Started");
 	}
