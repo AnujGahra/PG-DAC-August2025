bd35b096b90318e74e21947fb5e53c58e145ba41
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-11 20:42:19 +0530

    Spring Boot => SpringBoot Data JPA JpaRepository

--- TargetTechnology/Java_Based/SpringBoot/SB-2/src/main/java/com/example/demo/Sb2Application.java
+++ TargetTechnology/Java_Based/SpringBoot/SB-2/src/main/java/com/example/demo/Sb2Application.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Sb2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Sb2Application.class, args);
+		ConfigurableApplicationContext ac = SpringApplication.run(Sb2Application.class, args);
 		
 		System.out.println("Application Started");
 	}
--- TargetTechnology/Java_Based/SpringBoot/SB-2/src/main/java/com/example/demo/entities/EmployeeEntity.java
+++ TargetTechnology/Java_Based/SpringBoot/SB-2/src/main/java/com/example/demo/entities/EmployeeEntity.java
@@ -1,5 +1,23 @@
 package com.example.demo.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeEntity {
+	
+	
+	@Id
+	private int eid;
+	private String ename;
+	private String eaddress;
+	private int esalary;
 
 }
