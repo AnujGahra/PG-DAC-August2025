cf4959470dfafbb2f013d74d8d9f20d271100fc6
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-12 10:40:38 +0530

    Service Class

--- TargetTechnology/Java_Based/SpringBoot/sb-3/src/main/java/com/example/demo/controller/MyController.java
+++ TargetTechnology/Java_Based/SpringBoot/sb-3/src/main/java/com/example/demo/controller/MyController.java
@@ -1,7 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.dto.EmployeeDTO;
 
 @Controller
 public class MyController {
@@ -18,6 +24,19 @@ public class MyController {
 	public String registrationView() {
 		
 		return "registration";
+	}
+	
+	
+	
+	@PostMapping("save")
+	public String save(@ModelAttribute EmployeeDTO employeeDTO, RedirectAttributes model) {
+		
+		System.out.println(employeeDTO);
+//		model.addAttribute("msg", "Data saved SuccessFully");
+		model.addFlashAttribute("msg", "Data saved SuccessFully");
+
+		return "redirect:/registration";
+		
 	}
 
 }
