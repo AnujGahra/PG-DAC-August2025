cd401b3fcab3037ef8d5f7f3e530ffd43a8d2475
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-12 10:12:55 +0530

    SpringBoot MVC

---
+++ TargetTechnology/Java_Based/SpringBoot/sb-3/src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+	
+	
+	@GetMapping(path = {"index", "home"})
+	public String WelcomeHome() {
+		
+		return "index";
+	}
+
+}
