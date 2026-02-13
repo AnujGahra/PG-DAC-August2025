82e7506b34348cee5be46e81bad5de7f0e0d0c05
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-13 11:59:05 +0530

    Employee Entity

--- TargetTechnology/Java_Based/SpringBoot/SpringBoot-1/src/main/java/com/example/demo/SpringBoot1Application.java
+++ TargetTechnology/Java_Based/SpringBoot/SpringBoot-1/src/main/java/com/example/demo/SpringBoot1Application.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBoot1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot1Application.class, args);
+		ConfigurableApplicationContext ac = SpringApplication.run(SpringBoot1Application.class, args);
 	}
 
 }
--- TargetTechnology/Java_Based/SpringBoot/SpringBoot-1/src/main/java/com/example/demo/reposittory/Crud.java
+++ TargetTechnology/Java_Based/SpringBoot/SpringBoot-1/src/main/java/com/example/demo/reposittory/Crud.java
@@ -1,5 +1,20 @@
 package com.example.demo.reposittory;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
 public class Crud {
+	
+	
+	@Autowired
+	private EntityManager entityManager;
+	
+	public void save() {
+		entityManager.persist(null);
+	}
 
 }
