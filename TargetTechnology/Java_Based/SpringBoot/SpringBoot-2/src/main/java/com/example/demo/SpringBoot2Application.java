49a0dd4dfcefc3a1bb4eb24afdd54c8e56e1cc99
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-13 13:16:23 +0530

    SpringBoot Data JPA CrudRepository

--- TargetTechnology/Java_Based/SpringBoot/SpringBoot-2/src/main/java/com/example/demo/SpringBoot2Application.java
+++ TargetTechnology/Java_Based/SpringBoot/SpringBoot-2/src/main/java/com/example/demo/SpringBoot2Application.java
@@ -10,6 +10,12 @@ import com.example.demo.repo.MyCrudRepository;
 @SpringBootApplication
 public class SpringBoot2Application {
 
+    private final MyCrudRepository myCrudRepository;
+
+    SpringBoot2Application(MyCrudRepository myCrudRepository) {
+        this.myCrudRepository = myCrudRepository;
+    }
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ac = SpringApplication.run(SpringBoot2Application.class, args);
 		
@@ -23,10 +29,14 @@ public class SpringBoot2Application {
 //		all.forEach(n->System.out.println(n));
 		
 		
-		Iterable<Employee> all = bean.findAll();
-		for(Employee e : all) {
-			System.out.println(e);
-		}
+//		Iterable<Employee> all = bean.findAll();
+//		for(Employee e : all) {
+//			System.out.println(e);
+//		}  
+		
+		
+//		Custom read Data
+		bean.readMyData().forEach(n->System.out.println(n));
 		
 		
 	}
--- TargetTechnology/Java_Based/SpringBoot/SpringBoot-2/src/main/java/com/example/demo/repo/MyCrudRepository.java
+++ TargetTechnology/Java_Based/SpringBoot/SpringBoot-2/src/main/java/com/example/demo/repo/MyCrudRepository.java
@@ -1,5 +1,8 @@
 package com.example.demo.repo;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.entity.Employee;
@@ -9,6 +12,12 @@ public interface MyCrudRepository extends CrudRepository<Employee, Integer> {
 	
 //	@Override
 //	Iterable<Employee> findAll();
+	
+	
+//	Custom Methods
+	
+	@Query(value = "select * from employee where name like 'm%' " ,nativeQuery = true)
+	public List<Employee> readMyData();
 	
 	
 
