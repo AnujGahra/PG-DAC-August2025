09b2e1695afe1d414b4eb00fd285bbc21c609747
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-13 12:56:49 +0530

    SpringBoot Data JPA CrudRepository

--- TargetTechnology/Java_Based/SpringBoot/SpringBoot-2/src/main/java/com/example/demo/SpringBoot2Application.java
+++ TargetTechnology/Java_Based/SpringBoot/SpringBoot-2/src/main/java/com/example/demo/SpringBoot2Application.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBoot2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot2Application.class, args);
+		ConfigurableApplicationContext ac = SpringApplication.run(SpringBoot2Application.class, args);
 	}
 
 }
