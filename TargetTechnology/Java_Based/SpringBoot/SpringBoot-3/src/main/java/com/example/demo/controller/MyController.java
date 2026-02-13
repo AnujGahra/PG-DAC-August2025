a14eca979240adda26ab7c5eac6b000ca10f0cc1
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-13 19:19:17 +0530

    insertData Controller

--- TargetTechnology/Java_Based/SpringBoot/SpringBoot-3/src/main/java/com/example/demo/controller/MyController.java
+++ TargetTechnology/Java_Based/SpringBoot/SpringBoot-3/src/main/java/com/example/demo/controller/MyController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MyController {
@@ -15,6 +16,11 @@ public class MyController {
 	
 	@GetMapping("/insert")
 	public String insertView() {
+		return "insert";
+	}
+	
+	@PostMapping("/insert")
+	public String insertData() {
 		return "insert";
 	}
 
