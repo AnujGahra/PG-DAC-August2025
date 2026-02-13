1847c648cf6878d1095604e02b00f30545502892
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-13 20:11:23 +0530

    SpringBoot MVC

--- TargetTechnology/Java_Based/SpringBoot/SpringBoot-4/src/main/java/com/example/demo/dto/EmployeeDTO.java
+++ TargetTechnology/Java_Based/SpringBoot/SpringBoot-4/src/main/java/com/example/demo/dto/EmployeeDTO.java
@@ -1,5 +1,13 @@
 package com.example.demo.dto;
 
+import java.time.LocalDate;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +17,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class EmployeeDTO {
 	
-	private int id;
-	private String name;
-	private String address;
-	private int salary;
-	private String date;
+	
+	@Min(value = 1, message = "Id must be greater than 0")
+    @Max(value = 9999, message = "Id must be less than 10000")
+    private int id;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+    private String name;
+
+    @NotBlank(message = "Address cannot be blank")
+    private String address;
+
+    @Min(value = 10000, message = "Salary must be at least 10000")
+    private int salary;
+
+    @PastOrPresent(message = "Date cannot be in the future")
+    private LocalDate date;
 
 }
