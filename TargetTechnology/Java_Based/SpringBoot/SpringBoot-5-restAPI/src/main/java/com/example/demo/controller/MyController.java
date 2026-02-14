37bf26a570c95dffa745f14ae7d6a58999d97966
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-15 00:09:30 +0530

    REST API Endpoints for insert data

--- TargetTechnology/Java_Based/SpringBoot/SpringBoot-5-restAPI/src/main/java/com/example/demo/controller/MyController.java
+++ TargetTechnology/Java_Based/SpringBoot/SpringBoot-5-restAPI/src/main/java/com/example/demo/controller/MyController.java
@@ -6,36 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.EmployeeDTO;
 import com.example.demo.service.MyService;
 
 //@Controller
 
 @RestController // = Controller + ResponseBody
+@RequestMapping("/api/")
 public class MyController {
-	
-	
+
 	@Autowired
 	private MyService myService;
-	
-	
+
 //	@ResponseBody  // used -> we not want to render view we want to data that's why it used
-	@GetMapping("api/readsales")
-	public String read() {
-		
-		
+	@GetMapping("readtest")
+	public String test() {
+
 		return myService.read();
 	}
-	
-	
-	@GetMapping("api/read")
-	public String readView(Model model) {
-		model.addAttribute("key", myService.read());
+
+	@PostMapping("insert")
+	public void insert(@RequestBody EmployeeDTO employeeDTO) {
 		
-		return "read";
+		System.out.println(employeeDTO);
+
 	}
-	
 
 }
