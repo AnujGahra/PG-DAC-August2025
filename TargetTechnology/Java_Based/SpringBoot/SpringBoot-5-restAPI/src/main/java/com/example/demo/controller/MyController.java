e476e905a601acfec11476ea569b46cfc7827f75
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-14 23:45:07 +0530

    Controller and service layer for salesData REST API

--- TargetTechnology/Java_Based/SpringBoot/SpringBoot-5-restAPI/src/main/java/com/example/demo/controller/MyController.java
+++ TargetTechnology/Java_Based/SpringBoot/SpringBoot-5-restAPI/src/main/java/com/example/demo/controller/MyController.java
@@ -4,12 +4,16 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.MyService;
 
-@Controller
+//@Controller
+
+@RestController // = Controller + ResponseBody
 public class MyController {
 	
 	
@@ -17,14 +21,21 @@ public class MyController {
 	private MyService myService;
 	
 	
-	@ResponseBody  // used -> we not want to render view we want to data that's why it used
+//	@ResponseBody  // used -> we not want to render view we want to data that's why it used
 	@GetMapping("api/readsales")
-	public Map<Integer, Integer> readSaleData() {
+	public String read() {
 		
 		
-		return myService.salesData();
+		return myService.read();
 	}
 	
+	
+	@GetMapping("api/read")
+	public String readView(Model model) {
+		model.addAttribute("key", myService.read());
+		
+		return "read";
+	}
 	
 
 }
--- TargetTechnology/Java_Based/SpringBoot/SpringBoot-5-restAPI/src/main/java/com/example/demo/service/MyService.java
+++ TargetTechnology/Java_Based/SpringBoot/SpringBoot-5-restAPI/src/main/java/com/example/demo/service/MyService.java
@@ -8,18 +8,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyService {
 	
-	public Map<Integer, Integer> salesData() {
-		Map<Integer, Integer> map = new HashMap();
-		map.put(1996, 2000);
-		map.put(2001, 2005);
-		map.put(2010, 2015);
-		map.put(2020, 2024);
-		map.put(2020, 45000);
-		map.put(2021, 50000);
-		map.put(2022, 55000);
-		map.put(2023, 60000);
-		
-		return map;
+	public String read() {
+		return "sucess";
 	}
 
 }
