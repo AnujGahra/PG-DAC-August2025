f3625abbea39909703875fa368805a3fb2bf8be9
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-14 22:55:44 +0530

    Controller and service layer for salesData REST API

--- TargetTechnology/Java_Based/SpringBoot/SpringBoot-5-restAPI/src/main/java/com/example/demo/controller/MyController.java
+++ TargetTechnology/Java_Based/SpringBoot/SpringBoot-5-restAPI/src/main/java/com/example/demo/controller/MyController.java
@@ -1,5 +1,30 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.service.MyService;
+
+@Controller
 public class MyController {
+	
+	
+	@Autowired
+	private MyService myService;
+	
+	
+	@ResponseBody  // used -> we not want to render view we want to data that's why it used
+	@GetMapping("api/readsales")
+	public Map<Integer, Integer> readSaleData() {
+		
+		
+		return myService.salesData();
+	}
+	
+	
 
 }
--- TargetTechnology/Java_Based/SpringBoot/SpringBoot-5-restAPI/src/main/java/com/example/demo/service/MyService.java
+++ TargetTechnology/Java_Based/SpringBoot/SpringBoot-5-restAPI/src/main/java/com/example/demo/service/MyService.java
@@ -3,11 +3,13 @@ package com.example.demo.service;
 import java.util.HashMap;
 import java.util.Map;
 
-@org.springframework.stereotype.Service
+import org.springframework.stereotype.Service;
+
+@Service
 public class MyService {
 	
-	public Map<Object, Object> salesData() {
-		Map<Object, Object> map = new HashMap();
+	public Map<Integer, Integer> salesData() {
+		Map<Integer, Integer> map = new HashMap();
 		map.put(1996, 2000);
 		map.put(2001, 2005);
 		map.put(2010, 2015);
