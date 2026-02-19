38a1ac876dd5e81d8dab8b62d02079e230d55ebb
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-19 18:53:45 +0530

    Configure Spring Security Basic Auth with role-based access using in-memory authentication

--- Web_Technology/Java_Based/Spring_Security/Basic-Authentication/src/main/java/com/codehunt/SecurityConfig.java
+++ Web_Technology/Java_Based/Spring_Security/Basic-Authentication/src/main/java/com/codehunt/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.codehunt;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @SuppressWarnings("deprecation")
@@ -16,11 +19,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http
 		.authorizeHttpRequests()
-		.antMatchers("/mamber").authenticated()
-		.antMatchers("/admin").authenticated()
+		.antMatchers("/member").hasRole("MEMBER")
+//		.authenticated()
+		.antMatchers("/admin").hasRole("ADMIN")
+//		.authenticated()
 		.anyRequest().permitAll()
 		.and()
-		.httpBasic();
+		.httpBasic()
+		.and()
+		.exceptionHandling()
+		.accessDeniedPage("/error/403");
+		
+	}
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.withUser("admin123")
+			.password(bCryptPasswordEncoder().encode("admin333"))
+			.roles("ADMIN")
+			.and()
+			.withUser("member123")
+			.password(bCryptPasswordEncoder().encode("member333"))
+			.roles("MEMBER");
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder () {
+		return new BCryptPasswordEncoder();
 	}
 
 }
