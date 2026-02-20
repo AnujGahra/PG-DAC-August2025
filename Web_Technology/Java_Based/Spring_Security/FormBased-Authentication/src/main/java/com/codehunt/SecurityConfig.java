751a415195f36f88a525b9f1f504d73a634ee99a
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-20 17:51:53 +0530

    Form Based Authentication

---
+++ Web_Technology/Java_Based/Spring_Security/FormBased-Authentication/src/main/java/com/codehunt/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.codehunt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+@Configuration
+public class SecurityConfig {
+
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests()
+			.antMatchers("/admin").authenticated()
+			.anyRequest().permitAll()
+			.and()
+			.formLogin();
+
+		return http.build();
+	}
+
+
+}
--- Web_Technology/Java_Based/Spring_Security/FormBased-Authentication/src/main/java/com/codehunt/controller/MyController.java
+++ Web_Technology/Java_Based/Spring_Security/FormBased-Authentication/src/main/java/com/codehunt/controller/MyController.java
@@ -1,5 +1,20 @@
 package com.codehunt.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MyController {
+	
+	
+	@GetMapping("/index")
+	public String welcomeHome() {
+		return "index";
+	}
+	
+	@GetMapping("/admin")
+	public String adminHome() {
+		return "admin";
+	}
 
 }
