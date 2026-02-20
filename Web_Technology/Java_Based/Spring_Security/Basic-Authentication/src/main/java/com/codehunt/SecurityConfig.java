a0bbed235e2c3e70c4c5c731af0cb09b80fd7974
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-20 16:19:49 +0530

    Implement role-based access control using SecurityFilterChain with Basic Authentication

--- Web_Technology/Java_Based/Spring_Security/Basic-Authentication/src/main/java/com/codehunt/SecurityConfig.java
+++ Web_Technology/Java_Based/Spring_Security/Basic-Authentication/src/main/java/com/codehunt/SecurityConfig.java
@@ -3,32 +3,77 @@ package com.codehunt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.codehunt.service.MyService;
 
 
-@SuppressWarnings("deprecation")
+//@SuppressWarnings("deprecation")
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//@EnableWebSecurity
+public class SecurityConfig{
 	
 	@Autowired
 	private MyService myService;
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		
+//		http
+//		.authorizeHttpRequests()
+//		.antMatchers("/member").hasRole("MEMBER")
+////		.authenticated()
+//		.antMatchers("/admin").hasRole("ADMIN")
+////		.authenticated()
+//		.anyRequest().permitAll()
+//		.and()
+//		.httpBasic()
+//		.and()
+//		.exceptionHandling()
+//		.accessDeniedPage("/error/403");
+//		
+//	}
+//	
+//	
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		
+////		In-DataBase Authentication from Database
+//		auth.userDetailsService(myService).passwordEncoder(bCryptPasswordEncoder());
+//		
+//		
+//		
+////		In-Memory Authentication 
+////		auth
+////			.inMemoryAuthentication()
+////			.withUser("admin123")
+////			.password(bCryptPasswordEncoder().encode("admin333"))
+////			.roles("ADMIN")
+////			.and()
+////			.withUser("member123")
+////			.password(bCryptPasswordEncoder().encode("member333"))
+////			.roles("MEMBER");
+//		
+//		
+//		
+//	}
+	
+	
+//	by SecurityFilterChain
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 		.authorizeHttpRequests()
 		.antMatchers("/member").hasRole("MEMBER")
-//		.authenticated()
 		.antMatchers("/admin").hasRole("ADMIN")
-//		.authenticated()
 		.anyRequest().permitAll()
 		.and()
 		.httpBasic()
@@ -36,31 +81,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.exceptionHandling()
 		.accessDeniedPage("/error/403");
 		
+		return http.build();
 	}
 	
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
 		
-//		In-DataBase Authentication from Database
-		auth.userDetailsService(myService).passwordEncoder(bCryptPasswordEncoder());
-		
+//		In-Memory
 		
 		
-//		In-Memory Authentication 
-//		auth
-//			.inMemoryAuthentication()
-//			.withUser("admin123")
-//			.password(bCryptPasswordEncoder().encode("admin333"))
-//			.roles("ADMIN")
-//			.and()
-//			.withUser("member123")
-//			.password(bCryptPasswordEncoder().encode("member333"))
-//			.roles("MEMBER");
+//		Database (DEFAULT)
 		
-		
+		return authConfig.getAuthenticationManager();
 		
 	}
+	
 	
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder () {
