9def0734f101559209e39431b8fd8e6342bcc310
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-24 19:46:57 +0530

    LinkedList Implementation

---
+++ TargetTechnology/DSA/LinkedList/linkedListImpl.java
@@ -0,0 +1,13 @@
+public class linkedListImpl {
+
+    public static class Node {
+        int data;
+    }
+
+    public static void main(String[] args) {
+
+        Node x = new Node();
+        System.out.println(x);
+        
+    }
+}
