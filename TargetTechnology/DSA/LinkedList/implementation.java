13f8ea5d96c3753f0b011c71edcc043ca7fced4a
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-24 23:31:28 +0530

    LinkedList Implementation

--- TargetTechnology/DSA/LinkedList/implementation.java
+++ TargetTechnology/DSA/LinkedList/implementation.java
@@ -1,5 +1,50 @@
 public class implementation {
-    
 
-    
+    // Node class
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // LinkedList class
+    static class LinkedList {
+        Node head = null;
+        Node tail = null;
+
+        void insertAtEnd(int val) {
+            Node temp = new Node(val);
+
+            if (head == null) {
+                head = temp;
+                tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+        }
+
+        void display() {
+            Node curr = head;
+            while (curr != null) {
+                System.out.print(curr.data + " -> ");
+                curr = curr.next;
+            }
+            System.out.println("null");
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+
+        list.display();
+    }
 }
