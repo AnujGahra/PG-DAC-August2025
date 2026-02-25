b4408dae7c9fc4871c9af5371c9842f3b42b70e6
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-26 00:49:12 +0530

    Sum Of Array

---
+++ TargetTechnology/TCS-NQT/Programming/sumOfArray.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class sumOfArray {
+
+
+    public static int sumArray(int[] arr, int size) {
+        int sum = 0;
+        for(int i = 0; i < size; i++) {
+            sum += arr[i];
+        }
+        return sum;
+
+    }
+    
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for(int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Sum of the array is: " + sumArray(arr, size));
+    }
+}
