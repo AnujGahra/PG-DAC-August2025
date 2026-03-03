23ceb3e200d99b9cc043a27140335b8178431494
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-04 01:07:54 +0530

    Pair in STL

---
+++ TargetTechnology/C++/STL/new/Pair.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<pair>
+using namespace std;
+
+int main() {
+    pair<string, int> p;
+    // Two method insert
+    // 1st
+    // p = make_pair("Satyarth", 21);
+    // 2nd
+    // p.first = "Satyarth";
+    // p.second = 21;
+
+    //  add name, age, weight in pair
+    pair<string, pair<int, int>> p;
+    p.first = "Satyarth";
+    p.second.first = 21;
+    p.second.second = 70;
+
+    // cout << p.first << " " << p.second << endl;
+
+    cout << p.first << " " << p.second.first << " " << p.second.second << endl;
+}
+
