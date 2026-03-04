f5933fb55981a64906e3671caf11a4f6c3226d9f
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-04 11:39:43 +0530

    Reverse Number

---
+++ TargetTechnology/DSA/Questions/Question1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    long long n;
+    cin >> n;
+
+
+    if(n == 0) {
+        cout << 0;
+        return 0;
+    }
+
+     while(n != 0) {
+        int digit = n % 10;
+        cout << digit;
+        n /= 10;
+     }
+
+     return 0;
+}
