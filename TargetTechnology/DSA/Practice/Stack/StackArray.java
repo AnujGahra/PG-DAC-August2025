5bd4d3e2fb8ae10b19156df086260b7d44ef68e7
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-06 15:39:54 +0530

    Stack implementation Using Array

---
+++ TargetTechnology/DSA/Practice/Stack/StackArray.java
@@ -0,0 +1,24 @@
+package Stack;
+
+public class StackArray {
+
+    int arr[];
+    int top;
+    int capacity;
+
+    StackArray(int size) {
+        arr = new int[size];
+        capacity = size;
+        top = -1;
+    }
+
+    
+
+
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
