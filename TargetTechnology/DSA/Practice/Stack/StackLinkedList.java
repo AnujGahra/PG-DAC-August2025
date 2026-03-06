1a9764188487734d686508ed86387d04e49f0bc6
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-06 16:30:55 +0530

    Stack implementation Using LinkedList

--- TargetTechnology/DSA/Practice/Stack/StackLinkedList.java
+++ TargetTechnology/DSA/Practice/Stack/StackLinkedList.java
@@ -2,4 +2,23 @@ package Stack;
 
 public class StackLinkedList {
     
+
+    class Node{
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node top;
+
+    void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+    }
+
 }
