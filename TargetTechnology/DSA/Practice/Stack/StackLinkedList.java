d9d51d003d0b4cfdc5550f6ce96718077a43ddaf
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-06 16:32:35 +0530

    Stack implementation Using LinkedList

--- TargetTechnology/DSA/Practice/Stack/StackLinkedList.java
+++ TargetTechnology/DSA/Practice/Stack/StackLinkedList.java
@@ -21,4 +21,42 @@ public class StackLinkedList {
         top = newNode;
     }
 
+
+    // pop
+    int pop() {
+        if(top == null) {
+            return -1;
+        }
+
+        int value = top.data;
+        top = top.next;
+        return value;
+    }
+
+        int peek(){
+
+        if(top == null){
+            return -1;
+        }
+
+        return top.data;
+    }
+
+    boolean isEmpty(){
+        return top == null;
+    }
+
+
+
+    public static void main(String[] args) {
+        StackLinkedList stack = new StackLinkedList();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Top element: " + stack.peek());
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Top element after pop: " + stack.peek());
+    }
+
 }
