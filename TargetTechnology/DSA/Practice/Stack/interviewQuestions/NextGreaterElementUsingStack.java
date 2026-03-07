beff0dda1ade084fedbce4aec3b9ebb844bb2eea
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-07 12:54:38 +0530

    NextGreaterElement using Stack

--- TargetTechnology/DSA/Practice/Stack/interviewQuestions/NextGreaterElementUsingStack.java
+++ TargetTechnology/DSA/Practice/Stack/interviewQuestions/NextGreaterElementUsingStack.java
@@ -1,8 +1,40 @@
 package interviewQuestions;
 
+import java.util.Stack;
+
 public class NextGreaterElementUsingStack {
+
+    public static int[] nextGreater(int[] arr) {
+
+        int n = arr.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && st.peek() <= arr[i]) {
+                st.pop();
+            }
+
+            if (st.isEmpty())
+                res[i] = -1;
+            else
+                res[i] = st.peek();
+
+            st.push(arr[i]);
+        }
+
+        return res;
+    }
 
     public static void main(String[] args) {
 
+        int[] arr = { 4, 5, 2, 25 };
+
+        int[] res = nextGreater(arr);
+
+        for (int i : res) {
+            System.out.print(i + " ");
+        }
     }
 }
