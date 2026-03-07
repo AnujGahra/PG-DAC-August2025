67047d0e6d88ffd6743794b6889152704cd6ee22
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-07 12:37:49 +0530

    NextGreaterElement using Array

--- TargetTechnology/DSA/Practice/Stack/interviewQuestions/NextGreaterElement.java
+++ TargetTechnology/DSA/Practice/Stack/interviewQuestions/NextGreaterElement.java
@@ -4,5 +4,27 @@ public class NextGreaterElement {
 
     public static void main(String[] args) {
 
+        int arr[] = { 4, 5, 2, 10, 8 };
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = -1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[i]) {
+                    res[i] = arr[j];
+                    break;
+                }
+            }
+        }
+
+        for (int n : arr) {
+            System.out.print(n + " ");
+        }
+
+        System.out.println();
+
+        for (int i : res) {
+            System.out.print(i + " ");
+        }
+
     }
 }
