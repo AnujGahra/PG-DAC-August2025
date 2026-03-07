3355b21f2f5eb2b9a753564cd7ae62a6ee1ee659
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-07 12:24:15 +0530

    Remove Consecutive Subsequences using Stack

--- TargetTechnology/DSA/Practice/Stack/interviewQuestions/RemoveConsecutiveSubsequences.java
+++ TargetTechnology/DSA/Practice/Stack/interviewQuestions/RemoveConsecutiveSubsequences.java
@@ -1,5 +1,40 @@
 package interviewQuestions;
 
+import java.util.Stack;
+
 public class RemoveConsecutiveSubsequences {
 
+
+    public static int[] remove(int[] arr) {
+
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0; i<n; i++) {
+            if(st.size() == 0 || st.peek() != arr[i]) {
+                st.push(arr[i]);
+            }
+            else if(arr[i] == st.peek()) {
+                if(i == n-1 || arr[i] != arr[i+1]) st.pop();
+            }
+        }
+
+        int[] res = new int[st.size()];
+        int m = res.length;
+        for(int i = m-1; i>=0; i--) {
+            res[i] = st.pop();
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 3, 3, 4, 5, 5, 6};
+
+        int[] res = remove(arr);
+
+        for(int i : res) {
+            System.out.print(i + " ");
+        }
+    }
 }
