627bb870b43c03d1ae3dc07f1d4fded3088f19d8
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-09 12:29:28 +0530

    Queue implementation using Array

--- TargetTechnology/DSA/Queues/QueueImplUsingAray.java
+++ TargetTechnology/DSA/Queues/QueueImplUsingAray.java
@@ -1,5 +1,12 @@
 public class QueueImplUsingAray {
 
+
+    public static class queueArray{
+
+        int front = -1; // queue mein front se element nikala jata h 
+        int rear = -1; // queue mein rear se element dala jata h
+    }
+
     public static void main(String[] args) {
 
     }
