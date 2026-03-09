90b741074976c92215d4b9d6ca1009e2c9e690af
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-09 12:45:52 +0530

    Queue implementation(add method implemented) using Array

--- TargetTechnology/DSA/Queues/QueueImplUsingAray.java
+++ TargetTechnology/DSA/Queues/QueueImplUsingAray.java
@@ -1,10 +1,24 @@
 public class QueueImplUsingAray {
 
+    public static class queueArray {
 
-    public static class queueArray{
-
-        int front = -1; // queue mein front se element nikala jata h 
+        int front = -1; // queue mein front se element nikala jata h
         int rear = -1; // queue mein rear se element dala jata h
+        int size = 0;
+
+        int[] arr = new int[100];
+
+        public void add(int val) {
+            if (rear == arr.length - 1) {
+                System.out.println("Queue is full");
+                return;
+            }
+
+            if (front == -1)
+                front = rear = 0;
+            else
+                arr[++rear] = val;
+        }
     }
 
     public static void main(String[] args) {
