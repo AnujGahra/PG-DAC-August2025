ead464d3ada43413ad8226404f70b8cc90272bac
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-09 14:31:49 +0530

    Circular Queue Implementation using Array...

--- TargetTechnology/DSA/Queues/CircularQueueArray.java
+++ TargetTechnology/DSA/Queues/CircularQueueArray.java
@@ -1,5 +1,27 @@
 public class CircularQueueArray {
 
+
+    public static class Cqa {
+        int front = -1;
+        int rear = -1;
+        int size = 0;
+
+        int[] arr = new int[8];
+
+
+        public void add(int val) throws Exception {
+            if(size == arr.length) {
+                throw new Exception("Queue if full");
+            }
+
+            else if(size == 0) {
+                front = rear = 0;
+                arr[0] = val;
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
 
     }
