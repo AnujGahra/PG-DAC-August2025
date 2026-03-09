05d6bbed747c4addab40b11dc42cb4b175a8186c
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-09 18:57:12 +0530

    Find Reminder without % operator

--- TargetTechnology/TCS-NQT/Programming/Number Theory/Reminder.java
+++ TargetTechnology/TCS-NQT/Programming/Number Theory/Reminder.java
@@ -1,14 +1,26 @@
 public class Reminder {
 
-
     public static int reminder(int n, int x) {
-        return (n - x*(n / x));
+        return (n - x * (n / x));
+    }
+
+    // second method
+    public static int getReminder(int n, int x) {
+        int num = n/4;
+
+        for(int i = 1; i<=num; i++) {
+            n = n - x;
+        }
+
+        return n;
     }
 
     public static void main(String[] args) {
-        int n = 14, x = 4;
+        int n = 15, x = 4;
 
         int result = reminder(n, x);
         System.out.println(result);
+
+        System.out.println(getReminder(n, x));
     }
 }
