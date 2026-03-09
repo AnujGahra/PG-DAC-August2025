e31050c4213c3672cab6a2cf8ebbf1cb0683853f
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-09 19:17:48 +0530

    Square Root

--- TargetTechnology/TCS-NQT/Programming/Number Theory/SquareRoot.java
+++ TargetTechnology/TCS-NQT/Programming/Number Theory/SquareRoot.java
@@ -1,4 +1,42 @@
 public class SquareRoot {
+
+
+
+    public static int root1(int n) {
+        int result = 1;
+
+        while(result*result <= n) {
+            result ++;
+        }
+
+        return result - 1;
+    }
+
+
+    // Using Binary Search
+    public static int root2(int n) {
+        int start = 0;
+        int end = n;
+        int ans = 0;
+
+        while(start <= end) {
+
+            int mid = (start + end) / 2;
+
+            if(mid * mid == n)
+                return mid;
+
+            if(mid * mid < n) {
+                ans = mid;
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
     
 
     public static void main(String[] args) {
@@ -6,12 +44,9 @@ public class SquareRoot {
 
         int n = 40;
 
-        int result = 1;
+        
 
-        while(result*result <= n) {
-            result++;
-        }
+        System.out.println(root2(n));
 
-        System.out.println(result - 1);
     }
 }
