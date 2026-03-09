2e66ee3b39172e2234a07a2d7527be13d4151dd4
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-09 11:52:52 +0530

    Queue Implementation

--- TargetTechnology/DSA/Queues/BasicSTL.java
+++ TargetTechnology/DSA/Queues/BasicSTL.java
@@ -10,7 +10,24 @@ public class BasicSTL {
         // LinkedList<Integer> ll = new LinkedList<>();
 
         // Queue<Integer> qu = new Queue<>(); // X
-            
+           
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+
+        System.out.println( q);
+        q.remove();
+        System.out.println(q);
+        q.poll();
+        System.out.println(q);
+
+        System.out.println(q.element());
+        System.out.println(q.peek());
         
     }
 }
