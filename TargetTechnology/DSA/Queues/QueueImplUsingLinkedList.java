0ab8499fa197baa3922276d740dd220e63b1329c
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-09 13:47:16 +0530

    Queue Implementation using LinkedList...

--- TargetTechnology/DSA/Queues/QueueImplUsingLinkedList.java
+++ TargetTechnology/DSA/Queues/QueueImplUsingLinkedList.java
@@ -1,5 +1,38 @@
 public class QueueImplUsingLinkedList {
 
+
+    public static class Node{
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static  class queueLL{
+        Node head = null;
+        Node tail = null;
+        int size = 0;
+
+        // method for add element
+        public void add(int x) {
+            Node temp = new Node(x);
+
+            if(size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+
+            size++;
+
+        }
+
+        // remove element method
+    }
+
     public static void main(String[] args) {
 
     }
