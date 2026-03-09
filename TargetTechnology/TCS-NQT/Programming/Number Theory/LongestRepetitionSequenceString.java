ed69991fc5004eaa5ef12e66468582daa6a47b57
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-09 23:24:37 +0530

    Longest consecutive repeating character length

--- TargetTechnology/TCS-NQT/Programming/Number Theory/LongestRepetitionSequenceString.java
+++ TargetTechnology/TCS-NQT/Programming/Number Theory/LongestRepetitionSequenceString.java
@@ -1,8 +1,22 @@
 public class LongestRepetitionSequenceString {
-    
 
     public static void main(String[] args) {
-        String str = "ATTCGGGA";
-        
+        String str = "aaabbcaaaa";
+
+        int count = 1;
+        int ans = 1;
+
+        for (int i = 1; i < str.length(); i++) {
+
+            if (str.charAt(i) == str.charAt(i - 1))
+                count++;
+            else
+                count = 1;
+
+            ans = Math.max(ans, count);
+        }
+
+        System.out.println(ans);
+
     }
 }
