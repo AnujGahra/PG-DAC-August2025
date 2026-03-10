5a40c85c5191e61509c2ee1b7acf14c0d0cd81de
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-11 01:07:13 +0530

    Binary Tree Implementation

--- TargetTechnology/DSA/Tree/implTress.java
+++ TargetTechnology/DSA/Tree/implTress.java
@@ -12,6 +12,22 @@ public class implTress {
         }
 
     }
+
+    // Display method
+    public static void display(Node root) {
+        if(root == null) return;
+        System.out.print(root.val + " -> ");
+        if(root.left != null) System.out.print(root.left.val + "  ");
+        if(root.right != null) System.out.print(root.right.val + "  ");
+        System.out.println();
+
+        display(root.left);
+        display(root.right);
+
+    }
+
+
+
     public static void main(String[] args) {
         Node root = new Node(2);
         Node a = new Node(4);
@@ -25,6 +41,9 @@ public class implTress {
         a.right = d;
         Node e = new Node(11);
         b.right = e;
+
+
+        display(root);
 
     }
 }
