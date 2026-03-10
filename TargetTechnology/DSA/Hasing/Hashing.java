148059d6dc173a790d7286485d411334f7281d32
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-10 11:44:13 +0530

    Hashmap

---
+++ TargetTechnology/DSA/Hasing/Hashing.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hashing {
+
+    static void HashMapMethods() {
+
+        Map<String, Integer> mp = new HashMap<>();
+
+        // Adding Elements in Hashmap
+        mp.put("Akash", 21);
+        mp.put("Arman", 16);
+        mp.put("Lav", 22);
+        mp.put("Mahi", 52);
+        mp.put("Anuj", 07);
+
+    }
+
+
+    public static void main(String[] args) {
+        HashMapMethods();
+    }
+}
