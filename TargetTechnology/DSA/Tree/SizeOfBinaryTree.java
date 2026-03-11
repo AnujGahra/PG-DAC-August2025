01c1cb1a1ddb06a62545bae9924fd8106dddd183
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-11 11:27:00 +0530

    Find node with max value

--- TargetTechnology/DSA/Tree/SizeOfBinaryTree.java
+++ TargetTechnology/DSA/Tree/SizeOfBinaryTree.java
@@ -1,9 +1,6 @@
 public class SizeOfBinaryTree {
 
-    
-
-
-    public static class Node{
+    public static class Node {
         int val;
         Node left;
         Node right;
@@ -13,51 +10,49 @@ public class SizeOfBinaryTree {
         }
     }
 
-
     // preorder Traversal
     public static void preorder(Node root) {
-        if(root == null) return;
+        if (root == null)
+            return;
         System.out.print(root.val + " ");
         preorder(root.left);
         preorder(root.right);
     }
 
-
-
     // Size of tree
     public static int size(Node root) {
-        if(root == null) return 0;
+        if (root == null)
+            return 0;
         return 1 + size(root.left) + size(root.right);
     }
 
-
     // Sum of tree
     public static int sum(Node root) {
-        if(root == null) return 0;
+        if (root == null)
+            return 0;
         return root.val + sum(root.left) + sum(root.right);
     }
 
     // Height of tree
     public static int height(Node root) {
-        if(root == null) return 1;
+        if (root == null)
+            return 1;
 
         return 1 + height(root.left) + height(root.right);
     }
 
-
     // Find node with max value
     public static int maxValue(Node root) {
-        if(root == null) return 0;
+        if (root == null)
+            return Integer.MIN_VALUE;
 
         int a = root.val;
         int b = maxValue(root.left);
         int c = maxValue(root.right);
 
+        return Math.max(a, Math.max(b, c));
 
-        return Math.max(a, Math.max(b,c));
-        
     }
-    
 
     public static void main(String[] args) {
         Node root = new Node(1);
@@ -72,17 +67,13 @@ public class SizeOfBinaryTree {
         Node e = new Node(6);
         b.left = e;
 
-
         preorder(root);
         System.out.println();
         System.out.println("Size of Binary Tree: " + size(root));
 
-
         System.out.println("Sum of tree: " + sum(root));
 
-
         System.out.println("Hieght of tree: " + height(root));
-
 
         System.out.println("Max.. Value of Tree: " + maxValue(root));
 
