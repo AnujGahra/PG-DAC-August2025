c0f74fb92df74cc7672090120652ca56b35557d1
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-11 13:08:57 +0530

    print nth Level Element of Tree

--- TargetTechnology/DSA/Tree/SizeOfBinaryTree.java
+++ TargetTechnology/DSA/Tree/SizeOfBinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SizeOfBinaryTree {
 
     public static class Node {
@@ -35,8 +38,10 @@ public class SizeOfBinaryTree {
 
     // Height of tree
     public static int height(Node root) {
-        if (root == null) return 0;
-        if(root.left == null && root.right == null) return 0;
+        if (root == null)
+            return 0;
+        if (root.left == null && root.right == null)
+            return 0;
         return 1 + Math.max(height(root.left), height(root.right));
     }
 
@@ -53,59 +58,78 @@ public class SizeOfBinaryTree {
 
     }
 
-
     // find the min value of tree
     public static int minValue(Node root) {
-        if(root == null) return Integer.MAX_VALUE;
+        if (root == null)
+            return Integer.MAX_VALUE;
 
         return Math.min(root.val, Math.min(minValue(root.left), minValue(root.right)));
     }
 
-
     // find the product of tree
     public static int product(Node root) {
-        if(root == null) return 1;
+        if (root == null)
+            return 1;
 
         return root.val * product(root.left) * product(root.right);
     }
 
-
     // Traversal Order
     // preOrder
     public static void preOrder(Node root) {
-        if(root == null) return;
+        if (root == null)
+            return;
         System.out.print(root.val + " ");
         preOrder(root.left);
         preOrder(root.right);
     }
 
-
     // InOrder Traversal
     public static void inOrder(Node root) {
-        if(root == null) return;
+        if (root == null)
+            return;
 
         inOrder(root.left);
         System.out.print(root.val + " ");
         inOrder(root.right);
-        
-    }
 
+    }
 
     // Post Order Traversal
     public static void postOrder(Node root) {
-        if(root == null) return;
+        if (root == null)
+            return;
         postOrder(root.left);
         postOrder(root.right);
         System.out.print(root.val + " ");
     }
 
-
     // print nth lever tree
     public static void nthLevel(Node root, int n) {
-        if(root == null) return;
-        if(n == 1) System.out.print(root.val + " ");
+        if (root == null)
+            return;
+        if (n == 1) {
+            System.out.print(root.val + " ");
+            return;
+        }
         nthLevel(root.left, n - 1);
         nthLevel(root.right, n - 1);
+    }
+
+    // Breadth First Search(BFS)
+    public static void bfs(Node root) {
+        Queue<Node> q = new LinkedList<>();
+        if (root != null)
+            q.add(root);
+        while (q.size() > 0) {
+            Node temp = q.peek();
+            if (temp.left != null)
+                q.add(temp.left);
+            if (temp.right != null)
+                q.add(temp.right);
+            System.out.print(temp.val + " ");
+            q.remove();
+        }
     }
 
     public static void main(String[] args) {
@@ -133,9 +157,7 @@ public class SizeOfBinaryTree {
 
         System.out.println("Max.. Value of Tree: " + maxValue(root));
 
-
         System.out.println("Min Value of Tree: " + minValue(root));
-
 
         System.out.println("Product of Tree: " + product(root));
 
@@ -158,12 +180,16 @@ public class SizeOfBinaryTree {
         nthLevel(root, 3);
 
         System.out.println();
-        // using for loop find level 
+        // using for loop find level
         int level = height(root) + 1;
-        for(int i = 0; i<= level; i++) {
+        for (int i = 0; i <= level; i++) {
             nthLevel(root, i);
             System.out.println();
         }
+
+        System.out.println();
+        System.out.print("After BFS Traversal: ");
+        bfs(root);
 
     }
 }
