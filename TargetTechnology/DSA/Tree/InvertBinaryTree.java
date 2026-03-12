26e8c1b6fa28b01595b9ae30faca30279164d118
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-12 11:50:18 +0530

    Symmetric Binary Tree

--- TargetTechnology/DSA/Tree/InvertBinaryTree.java
+++ TargetTechnology/DSA/Tree/InvertBinaryTree.java
@@ -24,7 +24,22 @@ public class InvertBinaryTree {
     }
 
     // Symmetric Tree
-    
+
+    public static boolean isSameTree(Node p, Node q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public boolean isSymmetric(Node root) {
+        if(root == null) return true;
+        root.left = invertTree(root.left);
+        return isSameTree(root.left, root.right);
+    }
 
 
    
