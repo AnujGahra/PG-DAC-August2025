dea4c5c6b37cd0d3c48a5da8bfdfe97e4b1c6678
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-14 18:00:43 +0530

    Find the 2nd non-repeating character from the result string

--- TargetTechnology/TCS-NQT/Programming/returnNonRepeatingElement.java
+++ TargetTechnology/TCS-NQT/Programming/returnNonRepeatingElement.java
@@ -1,11 +1,43 @@
 public class returnNonRepeatingElement {
 
-
     public static String removeVowel(String str) {
         StringBuilder result = new StringBuilder();
-    }
-    
 
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u' &&
+                    ch != 'A' && ch != 'E' && ch != 'I' && ch != 'O' && ch != 'U') {
+
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+
+    private static char secondNonRepeating(String str) {
+
+        int[] freq = new int[256];
+
+        // count frequency
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (freq[str.charAt(i)] == 1) {
+                count++;
+
+                if (count == 2) {
+                    return str.charAt(i);
+                }
+            }
+        }
+
+        return '-'; // if not found
+    }
 
     public static void main(String[] args) {
         String str1 = "art";
@@ -15,13 +47,13 @@ public class returnNonRepeatingElement {
 
         StringBuilder concat = new StringBuilder();
 
-        for(int i = 0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             // Alternate concatenation
-            if(i < str1.length()) {
+            if (i < str1.length()) {
                 concat.append(str1.charAt(i));
             }
 
-            if(i < str2.length()) {
+            if (i < str2.length()) {
                 concat.append(str2.charAt(i));
             }
         }
@@ -30,7 +62,11 @@ public class returnNonRepeatingElement {
         System.out.println("Alternate Concat: " + merged);
 
         String noVowel = removeVowel(merged);
+        System.out.println("After Removing Vowel: " + noVowel);
 
-        
+        char result = secondNonRepeating(noVowel);
+        System.out.println("2nd Non-Repeating Character: " + result);
+
     }
+
 }
