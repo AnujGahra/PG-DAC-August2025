b4987bc80fad32b87ca7b7dfabe9a39ce95571a3
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-16 18:16:27 +0530

    Main method and String

---
+++ TargetTechnology/TCS-NQT/Programming/MainMethodJava.java
@@ -0,0 +1,18 @@
+public class MainMethodJava {
+    
+
+    public static void main(int x) {
+        System.out.println("Value of X: " + x);
+    }
+
+
+    public static void main(String[] args) {
+
+        String str = "Anuj";
+        str = "Kumar";
+        // str[0] = 'N';
+        System.out.println(str);
+        System.out.println("This is main Method of Java Program...");
+        main(10);
+    }
+}
