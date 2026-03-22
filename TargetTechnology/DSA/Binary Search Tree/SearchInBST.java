db4b865db68f26569a395544d89bebce1a7f9473
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-22 21:09:37 +0530

    Binary Search Tree

---
+++ TargetTechnology/DSA/Binary Search Tree/SearchInBST.java
@@ -0,0 +1,48 @@
+public class SearchInBST {
+
+    // Node class
+    static class Node {
+        int val;
+        Node left, right;
+
+        Node(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+
+    // Search function
+    public static Node search(Node root, int val) {
+        if (root == null) return null;
+
+        if (root.val == val) return root;
+
+        if (val < root.val) {
+            return search(root.left, val);
+        } else {
+            return search(root.right, val);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Creating BST manually
+        Node root = new Node(10);
+        root.left = new Node(5);
+        root.right = new Node(15);
+        root.left.left = new Node(3);
+        root.left.right = new Node(7);
+        root.right.left = new Node(12);
+        root.right.right = new Node(18);
+
+        int key = 7;
+
+        Node result = search(root, key);
+
+        if (result != null) {
+            System.out.println("Value found: " + result.val);
+        } else {
+            System.out.println("Value not found");
+        }
+    }
+}
