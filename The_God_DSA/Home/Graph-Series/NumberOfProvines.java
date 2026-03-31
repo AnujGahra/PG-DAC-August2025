918cf08d85d845388fa2c84423dd371428da5a54
Anuj Kumar <anujgahra244304@gmail.com>
2026-03-31 23:41:17 +0530

    Number Of Provines

--- The_God_DSA/Home/Graph-Series/NumberOfProvines.java
+++ The_God_DSA/Home/Graph-Series/NumberOfProvines.java
@@ -1,6 +1,25 @@
 public class NumberOfProvines {
 
-    
+    public static int findCircleNum(int[][] isConnected) {
+        int count = 0;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (isConnected[i][i] == 1) {
+                count++;
+                dfs(isConnected, i);
+            }
+        }
+        return count;
+    }
+
+    private static void dfs(int[][] isConnected, int i) {
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1) {
+                isConnected[i][j] = 0;
+                isConnected[j][i] = 0;
+                dfs(isConnected, j);
+            }
+        }
+    }
     
 
 
