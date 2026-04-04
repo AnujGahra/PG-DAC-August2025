20a7e0d9267beaa2a6328d2255ec33904c8f13d0
Anuj Kumar <anujgahra244304@gmail.com>
2026-04-04 10:05:15 +0530

    Fibonacci using Tabulation

--- The_God_DSA/Home/DP...../FibSeries.java
+++ The_God_DSA/Home/DP...../FibSeries.java
@@ -12,6 +12,18 @@ public class FibSeries {
         int[] dp = new int[n+1];
         return fibo(n, dp);
     }
+
+
+    public static int fibTab(int n) {
+        if(n <= 1) return n;
+         int[] dp = new int[n+1];
+            dp[0] = 0; 
+            dp[1] = 1;
+            for(int i = 2; i <= n; i++) {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+            return dp[n];
+    }
     
 
 
