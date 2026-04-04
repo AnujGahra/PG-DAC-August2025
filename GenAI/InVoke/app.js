844a6a311d2c58e8bdf8f6e6f3ee02bd31d7e838
Anuj Kumar <anujgahra244304@gmail.com>
2026-04-05 02:25:34 +0530

    Handle command of LLM like How are You so you can Handle it

--- GenAI/InVoke/app.js
+++ GenAI/InVoke/app.js
@@ -3,19 +3,27 @@ import OpenAI from "openai";
 
 dotenv.config();
 
-const client = new OpenAI({
-    apiKey: process.env.GROQ_API_KEY,
-    baseURL: "https://api.groq.com/openai/v1",
-});
+async function run() {
+    const client = new OpenAI({
+        apiKey: process.env.GROQ_API_KEY,
+        baseURL: "https://api.groq.com/openai/v1",
+    });
 
+    const response = await client.responses.create({
+        model: "llama-3.3-70b-versatile",
+        input: [
+            {
+                role: "system",
+                content: "You are Jarvis Gahra, a smart assistant that helps answer questions and solve problems. You are helpful, creative, clever, and very friendly."
+            },
+            {
+                role: "user",
+                content: "who are you?"
+            }
+        ]
+    });
 
-const response = await client.responses.create({
-    model: "llama-3.3-70b-versatile",
-    input: "Explain the importance of fast language models",
-    
-});
+    console.log(response.output_text);
+}
 
-
-
-console.log(response.output_text);
-
+run();
