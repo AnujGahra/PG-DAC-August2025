44343ad083118f003d978508dff66f659be731f5
Anuj Kumar <anujgahra244304@gmail.com>
2026-04-05 02:40:36 +0530

    max_output_tokens

--- GenAI/InVoke/app.js
+++ GenAI/InVoke/app.js
@@ -3,17 +3,16 @@ import OpenAI from "openai";
 
 dotenv.config();
 
-async function run() {
-    const client = new OpenAI({
-        apiKey: process.env.GROQ_API_KEY,
-        baseURL: "https://api.groq.com/openai/v1",
-    });
+const client = new OpenAI({
+    apiKey: process.env.GROQ_API_KEY,
+    baseURL: "https://api.groq.com/openai/v1",
+});
 
-    const response = await client.responses.create({
+const response = await client.responses.create({
         temperature: 1, // Controls the randomness of the output. Higher values (e.g., 0.8) make the output more random, while lower values (e.g., 0.2) make it more focused and deterministic.
-        top_p: 1, // Controls the diversity of the output. Higher values (e.g., 0.9) allow for more diverse outputs, while lower values (e.g., 0.5) make the output more focused on the most likely options.
-        stop:'ga', // A string or array of strings that, when encountered in the output, w ill cause the model to stop generating further text.
-        max_output_tokens: '100', // The maximum number of tokens to generate in the output. This limits the length of the response.
+        // top_p: 1, // Controls the diversity of the output. Higher values (e.g., 0.9) allow for more diverse outputs, while lower values (e.g., 0.5) make the output more focused on the most likely options.
+        // stop:'ga', // A string or array of strings that, when encountered in the output, w ill cause the model to stop generating further text.
+        // max_output_tokens: '100', // The maximum number of tokens to generate in the output. This limits the length of the response.
         model: "llama-3.3-70b-versatile",
         input: [
             {
@@ -27,7 +26,4 @@ async function run() {
         ]
     });
 
-    console.log(response.output_text);
-}
-
-run();
+console.log(response.output_text);
