ffc23237100d0bf5a81ccf7593b7cefe4a2affb1
Anuj Kumar <anujgahra244304@gmail.com>
2026-04-06 23:23:29 +0530

    add addEventListerner function to Handle input

--- GenAI/Tool-Calling/frontend/script.js
+++ GenAI/Tool-Calling/frontend/script.js
@@ -1,1 +1,19 @@
-console.log("Working!!..");
+const input = document.querySelector("#message-Input");
+console.log(input);
+
+
+
+input.addEventListener("keyup",handleEnter);
+
+function  generate(text) {
+    
+}
+
+function handleEnter(e){
+    
+    if(e.key === "Enter"){
+        const text = input?.value.trim();
+        if(!text) return;
+        generate(text);
+    }
+}
