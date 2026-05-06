97f72a3716968bf51ce3c4a10a94c082f2dc6897
Anuj Kumar <anujgahra244304@gmail.com>
2026-05-06 12:30:15 +0530

    Reverse String Using For loop

--- TargetTechnology/Java_Based/DoPractice/ReverseString.java
+++ TargetTechnology/Java_Based/DoPractice/ReverseString.java
@@ -4,8 +4,15 @@ public class ReverseString {
         String orginal = "automation";
 
         // using StringBuilder
-        String reversed = new StringBuilder(orginal).reverse().toString();
+        // String reversed = new StringBuilder(orginal).reverse().toString();
+        // System.out.println("Original string: " + orginal);
+        // System.out.println("Reversed string: " + reversed);
+
+        String rev = "";
+        for(int i = orginal.length()-1; i>=0; i--) {
+            rev = rev + orginal.charAt(i);
+        }
         System.out.println("Original string: " + orginal);
-        System.out.println("Reversed string: " + reversed);
+        System.out.println("Reversed string: " + rev);
     }
 }
